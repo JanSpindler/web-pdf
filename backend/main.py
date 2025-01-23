59831456d1ee68ae18d70cd5a6becd5f974cb834
JanSpindler <JanSpindler>
2025-01-23 15:20:44 +0100

    able to upload file

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI, Depends, HTTPException, File, UploadFile
 from fastapi.responses import HTMLResponse
 import sqlite3
 import datetime
+import os
+
+
+UPLOAD_FOLDER = "uploads"
+
 
 app = FastAPI()
 
@@ -46,12 +51,11 @@ def create_session(db = Depends(get_db)):
 
     # Insert new session
     db_cursor.execute(f"INSERT INTO session (start_time) VALUES ('{start_time_str}');")
+    db.commit()
     session_id = db_cursor.lastrowid
     if session_id is None:
         db.rollback()
         raise HTTPException(status_code=400, detail="Failed to create session")
-    else:
-        db.commit()
 
     # Success, return session id
     return {"session_id": session_id}
@@ -59,11 +63,45 @@ def create_session(db = Depends(get_db)):
 
 @app.post("/api/upload")
 def upload_file(file: UploadFile, db = Depends(get_db)):
-    # Check wether file table exists
+    # TODO: Get session id from request
+    session_id = 1
+
+    # Check if session exists in database
+
+    # Check if file is a pdf
+    filename = file.filename
+    if not filename.endswith(".pdf"):
+        raise HTTPException(status_code=400, detail="File must end with .pdf")
+
+    # Check file size wether file size is over 256 MB
+    if file.size > 1024 * 1024 * 256:
+        raise HTTPException(status_code=400, detail="File size must be less than 256 MB")
+
+    # Store file
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    filepath = os.path.join(UPLOAD_FOLDER, filename)
+    file.seek(0)
+    with open(filepath, "wb") as os_file:
+        os_file.write(file.file.read())
+
+    # Verify if the file was written successfully
+    if not os.path.exists(filepath):
+        raise HTTPException(status_code=500, detail="Failed to write file to disk")
+
+    # Check if file table exists
     db_cursor = db.cursor()
     if not table_exists(db_cursor, "file"):
         db_cursor.execute("CREATE TABLE file (id INTEGER PRIMARY KEY, session_id INTEGER, name TEXT);")
         db.commit()
+
+    # Store file meta data references in database
+    db_cursor.execute(f"INSERT INTO file (session_id, name) VALUES ({session_id}, '{filename}');")
+    db.commit()
+    file_id = db_cursor.lastrowid
+    if file_id is None:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Failed to store file meta data in database")
 
     # Success,
     return {"filename": file.filename}
